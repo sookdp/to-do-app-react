4c585d3aa7013f1ea2d5fac20e84628df0ca2a0a
SokhnaDIOPD22011515 <smdbdiop@gmail.com>
2024-04-02 11:12:48 +0200

    Emoji picker focntionnel. Lorsque le l'on clique sur le bouton de choix d'emoji, le picker apparait et lorsque l'on sélectionne un emoji, celui-ci s'affiche sur le input d'entrée de tâche

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,64 +1,57 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Picker from "emoji-picker-react";
 import emojiLogo from "/src/assets/smile.png";
 
 export default function NewTask({ onAdd }) {
-  const [enteredTask, setEnteredTask] = useState("");
+    const [inputStr, setInputStr] = useState("");
+    const [showPicker, setShowPicker] = useState(false);
 
-    const [chosenEmoji, setChosenEmoji] = useState(false);
-    const [selectedEmoji, setSelectedEmoji] = useState(null);
-
-  function handleChange(event) {
-    setEnteredTask(event.target.value);
-  }
-
-  function handleClick() {
-    if (enteredTask.trim() === "") {
-      return;
-    }
-    onAdd(enteredTask);
-    setEnteredTask("");
-  }
-
-  function handleEmojiClick(){
-      setChosenEmoji(!chosenEmoji);
-  }
-
-    function onEmojiClick(event, emojiObject) {
-        setSelectedEmoji(emojiObject);
-        console.log(selectedEmoji);
-        setChosenEmoji(false);
+    function onEmojiClick (event, emojiObject){
+        const sym = event.unified.split("_");
+        const codeArray= [];
+        sym.forEach((el) => codeArray.push("0x" + el));
+        let emoji = String.fromCodePoint(...codeArray);
+        setInputStr((prevInput) => prevInput + emoji);
+        setShowPicker(false);
     }
 
-  return (
-    <div className="flex items-center gap-4">
-      <input
-        type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
-        onChange={handleChange}
-        value={enteredTask}
-      />
-        <button onClick={handleEmojiClick}>
-            <img src={emojiLogo}
-                 className="w-10 h-10" alt="Emoji Button"/>
-        </button>
-        {chosenEmoji && (
-            <div
-                className="absolute top-80 left-15 z-20">
+    const handleEmojiClick = () => {
+        setShowPicker(!showPicker);
+    };
+
+    const handleChange = (event) => {
+        setInputStr(event.target.value);
+    };
+
+    const handleClick = () => {
+        onAdd(inputStr);
+        setInputStr("");
+    };
+
+    return (
+        <div className="flex items-center gap-4">
+            <input
+                className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+                value={inputStr}
+                onChange={handleChange}
+            />
+            <button onClick={handleEmojiClick}>
+                <img
+                    src={emojiLogo}
+                    className="w-10 h-10"
+                    alt="Emoji Button"
+                />
+            </button>
+            {showPicker && (<div className="absolute top-80 left-15 z-20">
                 <Picker
                     height="30vw"
                     width="25vw"
                     onEmojiClick={onEmojiClick}
                 />
             </div>)}
-      <button
-        className="text-stone-700 hover:text-red-950"
-        onClick={handleClick}
-      >
-        Add Task
-      </button>
-        {selectedEmoji && <div>{selectedEmoji.emoji}</div>}
-    </div>
-  );
+            <button onClick={handleClick} className="text-stone-700 hover:text-stone-500">
+                Add Task
+            </button>
+        </div>
+    );
 }
